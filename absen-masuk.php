1f0aeb92515995c4257c88d3084331d995151cf0
Delano <del.191006@gmail.com>
2023-08-16 19:18:04 +0700

    UI absen-masuk and logic

--- absen-masuk.php
+++ absen-masuk.php
@@ -1,17 +1,131 @@
-<?php ?>
+<?php 
+session_start();
+
+if( !isset($_SESSION["login"])) {
+  header("Location: login.php");
+  exit;
+}
+
+require 'functions/functions.php';
+
+date_default_timezone_set("Asia/Jakarta");
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="stylesheet" href="style/dashboard.css">
 
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 
-
-  <title>Document</title>
+    <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <title>Dashboard</title> 
 </head>
 <body>
-  
+    <nav class="sidebar close">
+        <header>
+            <div class="image-text">
+                <span class="image">
+                    <!--<img src="logo.png" alt="">-->
+                </span>
+
+                <div class="text logo-text">
+                    <span class="name">Dashboard</span>
+                    <span class="profession"><?= $_SESSION["login"];?></span>
+                </div>
+            </div>
+
+            <i class='bx bx-chevron-right toggle'></i>
+        </header>
+
+        <div class="menu-bar">
+            <div class="menu">
+                <ul class="menu-links p-0">
+                    <li class="nav-link">
+                        <a href="dashboardUser.php">
+                            <i class='bx bx-home-alt icon' ></i>
+                            <span class="text nav-text">Dashboard</span>
+                        </a>
+                    </li>
+
+                    <li class="nav-link">
+                        <a href="absen-masuk.php">
+                            <i class='bx bx-time bx-rotate-270 icon' ></i>
+                            <span class="text nav-text">Absensi Masuk</span>
+                        </a>
+                    </li>
+
+                    <li class="nav-link">
+                        <a href="absen-pulang.php">
+                            <i class='bx bx-time-five icon'></i>
+                            <span class="text nav-text">Absensi Pulang</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="bottom-content">
+                <li class="">
+                    <a href="logout.php">
+                        <i class='bx bx-log-out icon' ></i>
+                        <span class="text nav-text">Logout</span>
+                    </a>
+                </li>
+            </div>
+        </div>
+
+    </nav>
+
+    <section class="home">
+        <div class="container">
+          <h1 class="mt-5 text">Silahkan absen, <?= $_SESSION["login"];?></h1>
+          <div class="card ms-5 me-5">
+            <div class="card-body">
+              <form action="" method="post">
+              <div class="mb-3 mt-5">
+              <div class="row">
+                <div class="col">
+                  <label class="ms-5">Nama Siswa PKL</label>
+                </div>
+                <div class="col">
+                  <input type="text" class="form-control" name="username" value="<?=$_SESSION["login"] ?>"disabled>
+                </div>
+              </div>
+              <div class="row mt-3">
+                <div class="col">
+                  <label class="ms-5">Waktu Absensi Masuk</label>
+                </div>
+                <div class="col">
+                  <input type="datetime-local" class="form-control" value="<?= date("Y-m-d H:i:s")?>"disabled> 
+                </div>
+              </div>
+              <div class="mx-5 mt-4">
+                <button type="submit" class="btn btn-primary">Absen</button>
+              </div>
+              </form>
+            </div>
+          </div>
+    </section>
+
+    <script>
+    const body = document.querySelector('body'),
+      sidebar = body.querySelector('nav'),
+      toggle = body.querySelector(".toggle");
+
+
+toggle.addEventListener("click" , () =>{
+    sidebar.classList.toggle("close");
+})
+;
+    </script>
+
 </body>
 </html>
--- functions/functions.php
+++ functions/functions.php
@@ -41,5 +41,16 @@ function register($data){
   return mysqli_affected_rows($conn);
 }
 
-//function dashboard
+//function read
+function query($query){
+  global $conn;
+
+  $result = mysqli_query($conn,$query);
+  $rows = [];
+
+  while($row = mysqli_fetch_assoc($result)){
+    $rows[] = $row;
+  }
+  return $rows;
+}
 ?>
