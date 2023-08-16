246d1f4dd9dc0fe2b4f7f7557cb0f792d549ddab
Delano <del.191006@gmail.com>
2023-08-16 10:09:05 +0700

    UI register

--- register.php
+++ register.php
@@ -15,39 +15,48 @@ if(isset($_POST["register"])){
 ?>
 
 
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Halaman Registrasi</title>
-</head>
-<body>
-  <h1>Halaman Registrasi</h1>
-  <style>
-    label {
-     display: block; 
-    }
-  </style>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="style/style.css">
+    <title>slkfmslm</title>
+  </head>
+  <body>
+  <div class="position-absolute top-0 start-50 translate-middle-x">
+    <div class="form-container">
+      <div class="d-flex align-items-center">
+        <div class="flex-shrink-0">
+          <img class="img-register" src="img/register-img.png" alt="...">
+        </div>
+        
+        <div class="flex-grow-1 ms-5">
+          <h4 class="login-title">Create your account</h4>
+          <form action="" method="post">
+            <div class="mb-3">
+              <label for="username" class="form-label">Username</label>
+              <input type="text" name="username" class="form-control" id="username"  placeholder="Create your Username here" required>
+            </div>
+      
+            <div class="mb-3">
+              <label for="password" class="form-label">Password</label>
+              <input type="password" name="password" class="form-control" id="password" placeholder="Create your Password here" required>
+            </div>
 
-  <form action="" method="post">
-    <ul>
-      <li>
-        <label for="username">Masukan Username : </label>
-        <input type="text" name="username" id="username">
-      </li>
-      <li>
-        <label for="password">Masukan Password : </label>
-        <input type="password" name="password" id="password">
-      </li>
-      <li>
-        <label for="confirmPassword">Konfirmasi Password : </label>
-        <input type="password" name="confirmPassword" id="confirmPassword">
-      </li>
-      <li>
-        <button type="submit" name="register">Registrasi!</button>
-      </li>
-    </ul>
-  </form>
-</body>
+            <div class="mb-3">
+              <label for="confirmPassword" class="form-label">Confirm Password</label>
+              <input type="Password" name="confirmPassword" class="form-control" id="confirmPassword"  placeholder="Confirm your Password here" required>
+            </div>
+            <div class="mb-3">
+            <button button type="submit" class="btn btn-primary" name="register">Register!</button>
+            </div>
+          </form> 
+        </div>
+      </div>
+    </div>
+  </div>
+  </body>
 </html>
