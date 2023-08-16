2e2988c22dbc5c89b1811fdf1b9124b227807bcc
Delano <del.191006@gmail.com>
2023-08-16 14:42:34 +0700

    menambahkan fitur absen masuk

--- dashboardUser.php
+++ dashboardUser.php
@@ -16,7 +16,7 @@ require 'functions/functions.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
-    <link rel="stylesheet" href="style//dashboard.css">
+    <link rel="stylesheet" href="style/dashboard.css">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 
