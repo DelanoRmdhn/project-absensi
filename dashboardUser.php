2b32d67a18f385d721b89ed1aed761fe37e7b798
Delano <del.191006@gmail.com>
2023-08-25 14:18:11 +0700

    Membuat Layar Dahsboard Responsive

--- dashboardUser.php
+++ dashboardUser.php
@@ -17,6 +17,7 @@ require 'functions/functions.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
     <link rel="stylesheet" href="style/dashboard.css">
+    <link rel="stylesheet" href="style/queriesDashboard.css">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 
