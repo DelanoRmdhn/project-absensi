00a43ba2ff7eb0a40d0f40db129f44c54035d1f5
Delano <del.191006@gmail.com>
2023-08-25 13:34:37 +0700

    backup Style

--- absen-pulang.php
+++ absen-pulang.php
@@ -35,7 +35,7 @@ if(isset($_POST["absenPulang"])){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
-    <link rel="stylesheet" href="style/dashboard.css">
+    <link rel="stylesheet" href="style/backupDashboard.css">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 
